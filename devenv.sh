ee3bdfffb5ce5f593619c00ffb271aa3304e3889
Przemysław Pająk <office@fearlessspider.com>
2024-02-14 17:31:20 +0100

    add all option

--- devenv.sh
+++ devenv.sh
@@ -10,9 +10,9 @@ elif [ "$unamestr" = 'Darwin' ]; then
 	platform='darwin'
 fi
 
-read -p "What programming language do you need(separated by a space)? " p_language
+read -p "What programming language do you need(separated by a space - all for all available languages)? " p_language
 
-read -p "What database do you need(separated by a space)? " database
+read -p "What database do you need(separated by a space - all for all available databases)? " database
 
 if [ "$platform" = "linux" ]; then
 	distro=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
@@ -22,62 +22,62 @@ if [ "$platform" = "linux" ]; then
 		sudo pacman -Su
 		sudo pacman -S base-devel git curl openssl readline xz zlib libtool automake
 
-		if [[ "$p_language" = *"python"* ]]; then
+		if [[ "$p_language" = *"python"* || "$p_language" = "all" ]]; then
 			sudo pacman -S python python-pip
 		fi
 
-		if [[ "$p_language" = *"erlang" || "$p_language" = *"elixir"* ]]; then
+		if [[ "$p_language" = *"erlang" || "$p_language" = *"elixir"* || "$p_language" = "all" ]]; then
 			sudo pacman -S erlang elixir
 			yay -S rebar3 elixir-ls
 		fi
 
-		if [[ "$p_language" = *"ruby"* ]]; then
+		if [[ "$p_language" = *"ruby"* || "$p_language" = "all" ]]; then
 			sudo pacman -S ruby
 			yay -S rbenv
 		fi
 
-		if [[ "$p_language" = *"rust"* ]]; then
+		if [[ "$p_language" = *"rust"* || "$p_language" = "all" ]]; then
 			sudo pacman -S rust rust-analyzer
 		fi
 
-		if [[ "$p_language" = *"go"* ]]; then
+		if [[ "$p_language" = *"go"* || "$p_language" = "all" ]]; then
 			sudo pacman -S go gopls
 		fi
 
-		if [[ "$p_language" = *"lua"* ]]; then
+		if [[ "$p_language" = *"lua"* || "$p_language" = "all" ]]; then
 			sudo pacman -S lua luarocks lua-language-server
 		fi
 
-		if [[ "$p_language" = *"r-lang"* ]]; then
+		if [[ "$p_language" = *"r-lang"* || "$p_language" = "all" ]]; then
 			yay -S r-rlang
 		fi
 
-		if [[ "$p_language" = *"javascript"* || "$p_language" = *"typescript"* ]]; then
+		if [[ "$p_language" = *"javascript"* || "$p_language" = *"typescript"* || "$p_language" = "all" ]]; then
 			sudo pacman -S nodejs npm prettier
 			yay -S nvm
 		fi
 
-		if [[ "$p_language" = *"typescript"* ]]; then
+		if [[ "$p_language" = *"typescript"* || "$p_language" = "all" ]]; then
 			sudo pacman -S typescript
 		fi
 
-		if [[ "$p_language" = *"haskell"* ]]; then
+		if [[ "$p_language" = *"haskell"* || "$p_language" = "all" ]]; then
 			sudo pacman -S ghc haskell-language-server
 		fi
 
-		if [[ "$p_language" = *"perl"* ]]; then
+		if [[ "$p_language" = *"perl"* || "$p_language" = "all" ]]; then
 			sudo pacman -S perl perl-perl-languageserver
 		fi
 
-		if [[ "$p_language" = *"java"* ]]; then
+		if [[ "$p_language" = *"java"* || "$p_language" = "all" ]]; then
 			sudo pacman -S jdk-openjdk gradle
 		fi
 
-		if [[ "$database" = *"postgresql"* ]]; then
+		if [[ "$database" = *"postgresql"* || "$database" = "all" ]]; then
 			sudo pacman -S postgresql
 		fi
 
-		if [[ "$database" = *"sqlite"* ]]; then
+		if [[ "$database" = *"sqlite"* || "$database" = "all" ]]; then
 			sudo pacman -S sqlite
 		fi
 
@@ -91,16 +91,16 @@ if [ "$platform" = "linux" ]; then
 		sudo apt-get install build-essential git curl libfftw3-dev \
 			libasound2-dev libncursesw5-dev libpulse-dev libtool automake
 
-		if [[ "$p_language" = *"rust"* ]]; then
+		if [[ "$p_language" = *"rust"* || "$p_language" = "all" ]]; then
 			curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 			sudo apt-get install rust-analyzer
 		fi
 
-		if [[ "$p_language" = *"ruby"* ]]; then
+		if [[ "$p_language" = *"ruby"* || "$p_language" = "all" ]]; then
 			sudo apt-get install ruby-full
 		fi
 
-		if [[ "$p_language" = *"java"* ]]; then
+		if [[ "$p_language" = *"java"* || "$p_language" = "all" ]]; then
 			sudo apt-get install default-jdk gradle
 		fi
 		sudo apt-get install gum cava espeak-ng
@@ -109,39 +109,39 @@ if [ "$platform" = "linux" ]; then
 		sudo yum groupinstall "Development Tools"
 		sudo yum install readline readline-devel libtool automake zlib.i686 bzip2-libs.i686
 
-		if [[ "$p_language" = *"erlang"* || "$p_language" = *"elixir"* ]]; then
+		if [[ "$p_language" = *"erlang"* || "$p_language" = *"elixir"* || "$p_language" = "all" ]]; then
 			sudo yum install erlang elixir rebar
 		fi
 
-		if [[ "$p_language" = *"java"* ]]; then
+		if [[ "$p_language" = *"java"* || "$p_language" = "all" ]]; then
 			sudo yum install java
 		fi
 
-		if [[ "$p_language" = *"python"* ]]; then
+		if [[ "$p_language" = *"python"* || "$p_language" = "all" ]]; then
 			sudo yum install python-pip
 		fi
 
-		if [[ "$p_language" = *"go"* ]]; then
+		if [[ "$p_language" = *"go"* || "$p_language" = "all" ]]; then
 			sudo yum install go
 		fi
 
-		if [[ "$p_language" = *"rust"* ]]; then
+		if [[ "$p_language" = *"rust"* || "$p_language" = "all" ]]; then
 			sudo yum install rust rust-analyzer cargo
 		fi
 
-		if [[ "$p_language" = *"javascript"* ]]; then
+		if [[ "$p_language" = *"javascript"* || "$p_language" = "all" ]]; then
 			sudo yum install nodejs
 		fi
 
-		if [[ "$p_language" = *"lua"* ]]; then
+		if [[ "$p_language" = *"lua"* || "$p_language" = "all" ]]; then
 			sudo yum install lua luarocks
 		fi
 
-		if [[ "$p_language" = *"ruby"* ]]; then
+		if [[ "$p_language" = *"ruby"* || "$p_language" = "all" ]]; then
 			sudo yum install ruby ruby-devel rubygems
 		fi
 
-		if [[ "$database" = *"postgresql"* ]]; then
+		if [[ "$database" = *"postgresql"* || "$database" = "all" ]]; then
 			sudo yum install postgresql
 		fi
 
@@ -152,55 +152,55 @@ elif [ "$platform" = "darwin" ]; then
 	brew update && brew upgrade
 	brew install git curl libtool automake openssl readline xz zlib autoconf-archive
 
-	if [[ "$p_language" = *"python"* ]]; then
+	if [[ "$p_language" = *"python"* || "$p_language" = "all" ]]; then
 		brew install python
 	fi
 
-	if [[ "$p_language" = *"ruby"* ]]; then
+	if [[ "$p_language" = *"ruby"* || "$p_language" = "all" ]]; then
 		brew install ruby rbenv
 	fi
 
-	if [[ "$p_language" = *"erlang"* || "$p_language" = *"elixir"* ]]; then
+	if [[ "$p_language" = *"erlang"* || "$p_language" = *"elixir"* || "$p_language" = "all" ]]; then
 		brew install erlang elixir rebar3 elixir-ls
 	fi
 
-	if [[ "$p_language" = *"rust"* ]]; then
+	if [[ "$p_language" = *"rust"* || "$p_language" = "all" ]]; then
 		brew install rust rust-analyzer
 	fi
 
-	if [[ "$p_language" = *"lua"* ]]; then
+	if [[ "$p_language" = *"lua"* || "$p_language" = "all" ]]; then
 		brew install lua lua-language-server
 	fi
 
-	if [[ "$p_language" = *"go"* ]]; then
+	if [[ "$p_language" = *"go"* || "$p_language" = "all" ]]; then
 		brew install go
 	fi
 
-	if [[ "$p_language" = *"javascript"* || "$p_language" = *"javascript"* ]]; then
+	if [[ "$p_language" = *"javascript"* || "$p_language" = *"javascript"* || "$p_language" = "all" ]]; then
 		brew install node nvm typescript
 	fi
 
-	if [[ "$p_language" = *"go"* ]]; then
+	if [[ "$p_language" = *"go"* || "$p_language" = "all" ]]; then
 		brew install go gopls golangci-lint
 	fi
 
-	if [[ "$p_language" = *"haskell"* ]]; then
+	if [[ "$p_language" = *"haskell"* || "$p_language" = "all" ]]; then
 		brew install ghc haskell-language-server
 	fi
 
-	if [[ "$p_language" = *"perl"* ]]; then
+	if [[ "$p_language" = *"perl"* || "$p_language" = "all" ]]; then
 		brew install perl
 	fi
 
-	if [[ "$database" = *"postgresql"* ]]; then
+	if [[ "$database" = *"postgresql"* || "$database" = "all" ]]; then
 		brew install postgresql
 	fi
 
-	if [[ "$database" = *"sqlite"* ]]; then
+	if [[ "$database" = *"sqlite"* || "$database" = "all" ]]; then
 		brew install sqlite3
 	fi
 
-	if [[ "$p_language" = *"java"* ]]; then
+	if [[ "$p_language" = *"java"* || "$p_language" = "all" ]]; then
 		brew install gradle
 	fi
 
@@ -215,26 +215,26 @@ elif [ "$platform" = "darwin" ]; then
 	ln -s /usr/lib/libncurses.dylib /usr/local/lib/libncursesw.dylib
 fi
 
-if [[ "$p_language" = *"ruby"* ]]; then
+if [[ "$p_language" = *"ruby"* || "$p_language" = "all" ]]; then
 	sudo gem update
 	sudo gem install solargraph rubocop neovim tmuxinator
 fi
 
-if [[ "$p_language" = *"python"* ]]; then
+if [[ "$p_language" = *"python"* || "$p_language" = "all" ]]; then
 	curl https://pyenv.run | bash
 	pip install -U pip
 	pip install flake8 black isort cmake-language-server djlint pynvim
 fi
 
-if [[ "$p_language" = *"lua"* ]]; then
+if [[ "$p_language" = *"lua"* || "$p_language" = "all" ]]; then
 	sudo luarocks install luacheck
 fi
 
-if [[ "$p_language" = *"rust"* ]]; then
+if [[ "$p_language" = *"rust"* || "$p_language" = "all" ]]; then
 	cargo install selene stylua efmt bliss
 fi
 
-if [[ "$p_language" = *"go"* ]]; then
+if [[ "$p_language" = *"go"* || "$p_language" = "all" ]]; then
 	go install mvdan.cc/sh/v3/cmd/shfmt@latest
 	go install github.com/maaslalani/nap@main
 	go install github.com/DyegoCosta/snake-game@latest
@@ -246,14 +246,14 @@ fi
 
 gh extension install dlvhdr/gh-dash
 
-if [[ "$p_language" = *"javascript"* || "$p_language" = *"typescript"* ]]; then
+if [[ "$p_language" = *"javascript"* || "$p_language" = *"typescript"* || "$p_language" = "all" ]]; then
 	npm i -g eslint vscode-langservers-extracted markdownlint-cli write-good \
 		fixjson @fsouza/prettierd stylelint shopify-cli cross-env webpack \
 		sass serverless npm-run-all nativescript dockerfile-language-server-nodejs \
 		neovim gulp
 fi
 
-if [[ "$p_language" = *"java"* ]]; then
+if [[ "$p_language" = *"java"* || "$p_language" = "all" ]]; then
 	curl -s "https://get.sdkman.io" | bash
 	sdk install gradle
 fi
