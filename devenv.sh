27d3d91263f51d05b3dc339714d9d6e5daa5eef5
Przemysław Pająk <bespider@gmail.com>
2025-03-01 18:01:00 +0100

    add docker to ubuntu

--- devenv.sh
+++ devenv.sh
@@ -261,6 +261,23 @@ if [ "$platform" = "linux" ]; then
 			sudo apt-get install -y mongodb-org
 		fi
 
+		if [[ "$tools" = *"docker"* || "$tools" = "all" ]]; then
+			sudo install -m 0755 -d /etc/apt/keyrings
+			sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
+			sudo chmod a+r /etc/apt/keyrings/docker.asc
+			echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+			sudo apt update
+
+			# Install Docker engine and standard plugins
+			sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+			# Give this user privileged Docker access
+			sudo usermod -aG docker ${USER}
+
+			# Limit log size to avoid running out of disk
+			echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+		fi
+
 		if [[ "$tools" = *"cava"* || "$tools" = "all" ]]; then
 			sudo apt-get install espeak-ng cava
 		fi
